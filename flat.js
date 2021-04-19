c3e56c14c8c20e323220784b0e528683e9c6b8cf
Jakub Szczepaniak <jakub.szczepaniak@mtab.com>
2021-04-19 19:18:01 +0200

    Random.

--- flat.js
+++ flat.js
@@ -1,48 +1,161 @@
-window.wallGroups = [
+window.solidWalls = [
+  //lazienki i szafa
   {
-    color: '#000',
-    width: 10,
-    walls: [
-      { x: 400, y: 500 }, // door left
-      { x: -40 },
-      { y: -20 },
-      { x: -80 }, // wnęka  na lewo od drzwi
-      { y: 20, },
-      { x: -150 },
-      { y: -100 },
-      { x: 150 },
-      { y: 40 },
-      { y: -300 },
-      { x: 300 },
-      { y: 300 },
-      { y: -100 },
-      { x: 300 },
-      { y: 160 },
-      { x: -440 }
-    ]
+    x: 0,
+    y: 0,
+    h: 263
   },
   {
-    color: '#09f',
-    width: 5,
-    walls: [
-      { x: 210, y: 500 },
-      { y: -30 }
-    ]
+    x: 0,
+    y: 0,
+    w: 158
   },
   {
-    color: '#09f',
-    width: 5,
-    walls: [
-      { x: 210, y: 400 },
-      { y: 40 },
-      { x: 30 }
-    ]
+    x: 0,
+    y: 263,
+    w: 202
+  },
+  {
+    x: 202,
+    y: 263,
+    w: 148
+  },
+  {
+    x: 202,
+    y: 0,
+    w: 148
+  },
+  {
+    x: 350,
+    y: 0,
+    h: 90,
+    id: 'dml1'
+  },
+  {
+    x: 350,
+    y: 263,
+    h: -45
+  },
+  // pokoje dziecięce
+  {
+    x: 1200,
+    y: 0,
+    w: 258,
+  },
+  {
+    x: 1458,
+    y: 0,
+    h: 374
+  },
+  {
+    x: 1458,
+    y: 374,
+    w: -258
+  },
+  {
+    x: 1200,
+    y: 374,
+    w: -163,
+    id: 'p31'
+  },
+  {
+    x: 1037,
+    y: 374,
+    w: -152,
+  },
+
+  // pokoj 2
+  {
+    x: 1200,
+    y: 0,
+    w: -286,
+  },
+  {
+    x: 914,
+    y: 0,
+    h: 268 + 10, // + 10 dla wyrównania ścian
+    id: 'p21'
+  },
+
+  // wspolny korytarz
+  {
+    x: 885,
+    y: 374,
+    w: -252,
   },
 ]
 
-window.wallGroups.forEach((wallGroup) => {
-  wallGroup.walls.forEach((wall, index) => {
-    wall.x = wall.x || 0;
-    wall.y = wall.y || 0;
-  });
-})
+window.thinWalls = [
+  //lazienki i szafa
+  {
+    x: 158,
+    y: 0,
+    h: 45
+  },
+  {
+    x: 158,
+    y: 45,
+    w: 45
+  },
+  {
+    // TODO doesn't match
+    x: 203,
+    y: 0,
+    h: 98,
+    id: 'dl2'
+  },
+  {
+    x: 203,
+    y: 90,
+    w: 58,
+    id: 'dml2'
+  },
+  {
+    // TODO doesn't match
+    x: 202,
+    y: 263,
+    h: -75,
+    id: 'dl1'
+  },
+
+  // pokoje dziecięce
+  {
+    x: 1200,
+    y: 0,
+    h: 276,
+  },
+  {
+    x: 1200,
+    y: 276,
+    w: -163,
+    id: 'p32'
+  },
+  {
+    x: 1037,
+    y: 276,
+    w: -10, // todo
+    id: 'p22'
+  },
+];
+
+window.doors = [
+  {
+    from: 'dl1',
+    to: 'dl2'
+  },
+  {
+    from: 'dml1',
+    to: 'dml2',
+    rotate: -Math.PI * .5
+  },
+  {
+    from: 'p31',
+    to: 'p32',
+    // rotate: -Math.PI * .5
+  },
+  {
+    from: 'p21',
+    to: 'p22',
+    rotate: -Math.PI * .5
+  },
+];
--- main.js
+++ main.js
@@ -3,27 +3,57 @@ const ctx = flat.getContext('2d');
 flat.width = window.innerWidth;
 flat.height = window.innerHeight;
 
-flat.addEventListener('mousemove', (ev)=>{
-  coords.style.transform = `translate(${ev.pageX + 10}px,${ev.pageY + 10}px)`;
-  coords.textContent = `${ev.pageX}:${ev.pageY}`
-});
+ctx.fillStyle = '#000';
+ctx.font = '20px Arial';
 
-// ctx.lineCap = 'round';
-ctx.lineJoin = 'miter';
+const SOLID_WALL = 2;
+const THIN_WALL = 1;
 
-wallGroups.forEach(wallGroup => {
+solidWalls.forEach((wall) => {
+  ctx.fillRect(wall.x, wall.y, wall.w || SOLID_WALL, wall.h || SOLID_WALL);
+})
 
-  ctx.lineWidth = wallGroup.width;
-  ctx.strokeStyle = wallGroup.color;
+thinWalls.forEach((wall) => {
+  ctx.fillRect(wall.x, wall.y, wall.w || THIN_WALL, wall.h || THIN_WALL);
+})
+
+ctx.fillStyle = '#000';
+ctx.textAlign = 'center';
+ctx.textBaseline = 'middle';
+
+solidWalls.forEach((wall, index) => {
+  ctx.fillText(`${index +1}S ${Math.abs(wall.h || wall.w)}`, Math.floor((2 * wall.x + (wall.w || SOLID_WALL))/2), Math.floor((2 * wall.y + (wall.h || SOLID_WALL))/2) );
+})
+
+thinWalls.forEach((wall, index) => {
+  ctx.fillText(`${index +1}T ${Math.abs(wall.h || wall.w)}`, Math.floor((2 * wall.x + (wall.w || THIN_WALL))/2), Math.floor((2 * wall.y + (wall.h || THIN_WALL))/2) );
+})
+
+ctx.fillStyle = '#333';
+ctx.strokeStyle = '#333';
+ctx.setLineDash([10,10]);
+
+
+const allWalls = [...solidWalls, ...thinWalls];
+
+doors.forEach((door, index) => {
+  const from = allWalls.find((wall) => wall.id === door.from);
+  const to = allWalls.find((wall) => wall.id === door.to);
+
+  const fromX = from.x + (from.w || 0);
+  const fromY = from.y + (from.h || 0);
+  const toX = to.x + (to.w || 0);
+  const toY = to.y + (to.h || 0);
+  const distance = Math.floor(Math.hypot(fromX - toX, fromY - toY));
+
+
+  const angleRadians = Math.atan2(toY - fromY, toX - fromX) + (door.rotate || 0);
+
   ctx.beginPath();
-  ctx.moveTo(wallGroup.walls[0].x + .5, wallGroup.walls[0].y + .5);
-  wallGroup.walls.reduce(({ x = 0, y = 0 }, wall) => {
-    x += wall.x;
-    y += wall.y;
-    ctx.lineTo(x, y);
-    // ctx.strokeStyle = wall.color;
-    return {x,y}
-  });
+  ctx.moveTo(fromX, fromY);
+  ctx.arc(fromX, fromY, distance, angleRadians, angleRadians + Math.PI * 0.5);
+  ctx.lineTo(fromX, fromY);
   ctx.stroke();
 
-});
+  ctx.fillText(`${index +1}.d ${distance}`, Math.floor((fromX + toX)/2), Math.floor((fromY + toY)/2) );
+})
